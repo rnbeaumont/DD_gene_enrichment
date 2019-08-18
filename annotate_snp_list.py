19616b032e998075e7a2f9371a7affcc49baa5e6
Rob <r.beaumont@exeter.ac.uk>
2019-08-18 20:05:53 +0100

    Modified annotate_snp_list.py

--- annotate_snp_list.py
+++ annotate_snp_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
-f=open("BW_SNP_list_full","r");
-o=open("BW_SNP_list_full_distance_to_nearest_gene","w+");
-
+o=open("BW_SNP_list_full_distance_to_nearest_gene","w");
+with open("BW_SNP_list_full","r") as f:
+    line=f.readline()
+    line=line.rstrip()
+    print o.write(line+"\tdistance\tgene\n")
+    
 o.close();
-f.close();
